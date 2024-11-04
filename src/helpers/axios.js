7a5cebcd05a89636a0ac5a37faa108bc7980bf17
aarrechea <adrianarrechea@yahoo.com.ar>
2024-11-04 19:22:59 -0300

    remove cors headers

--- src/helpers/axios.js
+++ src/helpers/axios.js
@@ -10,8 +10,7 @@ const axiosService = axios.create({
     baseURL: "https://company-assessments.herokuapp.com/api", // "http://localhost:8000/api", 
     
     headers: {
-        "Content-Type": "application/json",       
-        'Access-Control-Allow-Origin':'*', 
+        "Content-Type": "application/json",         
     },
 });
 
